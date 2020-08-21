25f0491e0d6f1955694cdfd4f245260490b04a09
vikasgautam <vikas.gautam18@gmail.com>
2020-08-21 09:56:10 +0200

    Tests for checking folder creation

--- src/main/java/com/gautam/mantra/ProbeMain.java
+++ src/main/java/com/gautam/mantra/ProbeMain.java
@@ -6,31 +6,48 @@ import org.slf4j.LoggerFactory;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.InputStream;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 
 public class ProbeMain {
     public static Yaml yaml = new Yaml();
+    public static Logger logger = LoggerFactory.getLogger(ProbeMain.class.getName());
 
     public static void main(String[] args) {
 
+        // Load Cluster properties and configurations
         ClassLoader loader = Thread.currentThread().getContextClassLoader();
         InputStream inputStream = loader.getResourceAsStream("cluster-conf.yml");
 
         Map<String, String> properties = yaml.load(inputStream);
+        // print all loaded properties to console
+        printProperties(properties);
 
-        Logger logger = LoggerFactory.getLogger(ProbeMain.class.getName());
-
-        printProperties(properties, logger);
-
+        // begin probe - HDFS first
         ProbeHDFS hdfs = new ProbeHDFS();
         Boolean isReachable = hdfs.isReachable(properties);
-        logger.info("HDFS is reachable :: " + isReachable);
+
+        if(!isReachable){
+            logger.error("HDFS not reachable, skipping other HDFS Tests");
+        }
+        else {
+            logger.info("HDFS is reachable");
+
+            Boolean isCreateFolderWorking = hdfs.createFolder(properties);
+            if(isCreateFolderWorking){
+                logger.info("HDFS test folder successfully created !");
+            }
+            else {
+                logger.error("HDFS test folder cannot be created. exiting ...");
+            }
+        }
     }
 
-    private static void printProperties(Map<String, String> properties, Logger logger) {
+    /**
+     *
+     * @param properties cluster properties loaded from config file
+     */
+    private static void printProperties(Map<String, String> properties) {
         for (String key: properties.keySet()) {
             logger.info(key + " --> " + properties.get(key));
         }
--- src/main/java/com/gautam/mantra/hdfs/ProbeHDFS.java
+++ src/main/java/com/gautam/mantra/hdfs/ProbeHDFS.java
@@ -5,6 +5,8 @@ import com.gautam.mantra.commons.ProbeService;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
@@ -12,8 +14,14 @@ import java.util.Map;
 
 public class ProbeHDFS implements ProbeFileSystem, ProbeService {
 
-    public Boolean isReachable(Map<String, String> props) {
+    public static Logger logger = LoggerFactory.getLogger(ProbeHDFS.class.getName());
 
+    /**
+     * This method verifies if HDFS is reachable
+     * @param props cluster properties loaded from config file
+     * @return True if HDFS is reachable False otherwise
+     */
+    public Boolean isReachable(Map<String, String> props) {
         Configuration conf= new Configuration();
         conf.set("fs.defaultFS", props.get("hdfsPath"));
         conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
@@ -28,23 +36,63 @@ public class ProbeHDFS implements ProbeFileSystem, ProbeService {
         }
     }
 
-    public Boolean createFolder(String folderName) {
+    /**
+     * This method verifies if HDFS folder creation from Java API is working as expected
+     * @param props Cluster configuration as properties
+     * @return True if creating folder works as expected, False otherwise
+     */
+    public Boolean createFolder(Map<String, String> props) {
+
+        Configuration conf= new Configuration();
+        conf.set("fs.defaultFS", props.get("hdfsPath"));
+        conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
+        conf.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
+
+        try {
+            FileSystem fs = FileSystem.get(URI.create(props.get("hdfsPath")), conf);
+
+            if(fs.exists(new Path (props.get("testHDFSFolder")))){
+                logger.info("HDFS test folder already exists: Deleting it first");
+                if (deleteFolder(props)){
+                    logger.info("HDFS test folder successfully deleted !");
+                }
+                else {
+                    logger.error("Test folder cannot be deleted, exiting... ");
+                    return false;
+                }
+            }
+
+            fs.create(new Path (props.get("testHDFSFolder")));
+
+            return (fs.exists(new Path (props.get("testHDFSFolder"))));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public Boolean createFile(Map<String, String> props) {
         return null;
     }
 
-    public Boolean createFile(String fileName) {
+    @Override
+    public Boolean readFile(Map<String, String> props) {
         return null;
     }
 
-    public Boolean readFile(String fileName) {
+    @Override
+    public Boolean deleteFile(Map<String, String> props) {
         return null;
     }
 
-    public Boolean deleteFile(String fileName) {
+    @Override
+    public Boolean deleteFolder(Map<String, String> props) {
         return null;
     }
 
-    public Boolean updatePermissions(String fileName) {
+    @Override
+    public Boolean updatePermissions(Map<String, String> props) {
         return null;
     }
 }
