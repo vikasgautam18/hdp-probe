16b36d902d3d849bf702b9b28a122f51c40bf02f
vikasgautam <vikas.gautam18@gmail.com>
2020-09-09 11:34:54 +0200

    Spark job for writing to HDFS

---
+++ src/main/java/com/gautam/mantra/spark/Event.java
@@ -0,0 +1,42 @@
+package com.gautam.mantra.spark;
+
+
+import java.sql.Timestamp;
+
+public class Event {
+    String eventId;
+    Timestamp eventTs;
+
+    public Event(String s, java.sql.Timestamp timestamp) {
+        this.setEventId(s);
+        this.setEventTs(timestamp);
+    }
+
+    public Event() {
+
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public Timestamp getEventTs() {
+        return eventTs;
+    }
+
+    public void setEventTs(Timestamp eventTs) {
+        this.eventTs = eventTs;
+    }
+
+    @Override
+    public String toString() {
+        return "Events{" +
+                "eventId='" + eventId + '\'' +
+                ", eventTs=" + eventTs +
+                '}';
+    }
+}
--- src/main/java/com/gautam/mantra/spark/SparkHDFSProbe.java
+++ src/main/java/com/gautam/mantra/spark/SparkHDFSProbe.java
@@ -3,33 +3,39 @@ package com.gautam.mantra.spark;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.DataTypes;
-import org.apache.spark.sql.types.StructField;
-import org.apache.spark.sql.types.StructType;
 
-import static org.apache.spark.sql.types.DataTypes.TimestampType;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 
 public class SparkHDFSProbe {
 
     public static void main(String[] args) {
         Logger.getLogger("org").setLevel(Level.ERROR);
         SparkSession spark = SparkSession.builder()
-                .appName("HousePriceSolution")
+                .appName("spark-hdfs-test")
                 .master("local[1]").getOrCreate();
 
-        StructType schema = DataTypes.createStructType(new StructField[] {
-                DataTypes.createStructField("event_id", DataTypes.StringType, false),
-                DataTypes.createStructField("event_ts", TimestampType, false)
-        });
-
-        Dataset<Row> dataset = spark.read().schema(schema).csv(spark.emptyDataset(Encoders.STRING()));
-
-        dataset.printSchema();
+        Dataset<Row> dataset = generateDataSet(spark, 100);
         dataset.show();
 
+        dataset.write().csv("/user/vikgautammbb/spark-hdfs-test");
         spark.stop();
+    }
+
+    public static Dataset<Row> generateDataSet(SparkSession spark, int numRows){
+        ArrayList<Event> dataList = new ArrayList<>();
+
+        for (int i=1; i <= numRows; i++){
+            dataList.add(new Event("event-" + i, new Timestamp(System.currentTimeMillis())));
+        }
+
+        for (Event e: dataList
+             ) {
+            System.out.println(e.toString());
+        }
+
+        return spark.createDataFrame(dataList, Event.class);
     }
 }
