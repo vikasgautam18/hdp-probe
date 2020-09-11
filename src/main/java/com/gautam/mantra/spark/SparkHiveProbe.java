e9080f42cd5b9593f1e08476a33248a6d6438737
vikasgautam <vikas.gautam18@gmail.com>
2020-09-11 11:36:29 +0200

    hive probe

--- src/main/java/com/gautam/mantra/spark/SparkHiveProbe.java
+++ src/main/java/com/gautam/mantra/spark/SparkHiveProbe.java
@@ -37,8 +37,6 @@ public class SparkHiveProbe {
 
         SparkSession spark = SparkSession.builder()
                 .appName(properties.get("sparkHiveAppName"))
-                .enableHiveSupport()
-                .config("hive.metastore.warehouse.dir", "/apps/hive/warehouse")
                 .getOrCreate();
 
         Dataset<Row> dataset = generateDataSet(spark, Integer.parseInt(properties.get("sparkHiveNumRecords")));
