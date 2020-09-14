fdec6e84de8beb9e0926a713604aa4b8277b0c08
vikasgautam <vikas.gautam18@gmail.com>
2020-09-14 13:09:24 +0200

    reorganizing properties

--- src/main/java/com/gautam/mantra/spark/ProbeSpark.java
+++ src/main/java/com/gautam/mantra/spark/ProbeSpark.java
@@ -242,6 +242,7 @@ public class ProbeSpark {
         sparkConf.set("spark.driver.extraJavaOptions", properties.get("spark.driver.extraJavaOptions"));
         sparkConf.set("spark.yarn.am.extraJavaOptions", properties.get("spark.yarn.am.extraJavaOptions"));
         sparkConf.set("spark.driver.extraClassPath", properties.get("spark.driver.extraClassPath"));
+        sparkConf.set("spark.sql.hive.hiveserver2.jdbc.url", properties.get("hiveJDBCURL"));
 
 
         final String[] args = new String[]{
