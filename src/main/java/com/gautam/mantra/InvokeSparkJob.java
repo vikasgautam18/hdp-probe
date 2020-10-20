3355b799751602f68b791ff636bc5baeff4134d6
vikasgautam <vikas.gautam18@gmail.com>
2020-10-20 19:27:25 +0200

    YARN client for listing applications
    Signed-off-by: vikasgautam <vikas.gautam18@gmail.com>

--- src/main/java/com/gautam/mantra/InvokeSparkJob.java
+++ src/main/java/com/gautam/mantra/InvokeSparkJob.java
@@ -1,17 +1,30 @@
 package com.gautam.mantra;
 
 
+import org.apache.hadoop.yarn.client.api.YarnClient;
+import org.apache.hadoop.yarn.conf.YarnConfiguration;
+import org.apache.hadoop.yarn.exceptions.YarnException;
 import org.apache.spark.launcher.SparkAppHandle;
 import org.apache.spark.launcher.SparkLauncher;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 
 public class InvokeSparkJob {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, YarnException {
+        //withSparkLauncher();
+
+        YarnConfiguration yarnConfiguration = new YarnConfiguration();
+        YarnClient client = YarnClient.createYarnClient();
+        client.init(yarnConfiguration);
+
+        client.start();
+
+        System.out.println(client.getApplications().size());
+    }
+
+    public static void withSparkLauncher() throws IOException, InterruptedException {
         SparkLauncher sparkLauncher = new SparkLauncher();
-        sparkLauncher.redirectOutput(new File("/tmp/sparkpi-output.log"));
 
         SparkAppHandle handle = sparkLauncher
                 .setSparkHome("/usr/hdp/3.1.0.0-78/spark2")
